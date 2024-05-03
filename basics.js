19529ae5e97322e0ca4926a64e0373e942066f5e
devcollins42 <nnadicollins33@gmail.com>
2024-05-03 21:03:26 +0100

    basics.js

---
+++ basics.js
@@ -0,0 +1,12 @@
+/**
+ Concatenate two or more strings, one with your first name
+  and one with your last, to create a string with 
+  your full name as its value. For example, if your name is John Doe, 
+  think about how you can put 'John' and 'Doe' together to get 'John Doe'.
+
+ */
+
+let firstName = "Chibuzo";
+let lastName = "Nnadi";
+let fullName = firstName + "  " + lastName;
+console.log(fullName)
